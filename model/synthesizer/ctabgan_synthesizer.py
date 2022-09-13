0437478964a908a39da85b694dac8998c7f0e067
john <jwhero94@gmail.com>
2022-09-13 14:19:25 +0900

    init

--- model/synthesizer/ctabgan_synthesizer.py
+++ model/synthesizer/ctabgan_synthesizer.py
@@ -807,6 +807,8 @@ class CTABGANSynthesizer:
                         f.write(f'{i} {avg_loss}\n')
 
                     eval_pre = test_classifier(eval_data_data)
+                    eval_pre = torch.exp(eval_pre)
+                    eval_pre = eval_pre[:, 0] / (eval_pre[:, 0] + eval_pre[:, 1])
 
                     # eval_pre, eval_label = test_classifier(torch.from_numpy(eval_data.astype('float32')).to(self.device))
                     sorted_indices = torch.argsort(eval_pre)
